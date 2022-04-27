84c528cdc77e03080060d51aabb5156eff0ab69c
Nathan Benkemoun <nathan.ben-kemoun@student.isae-supaero.fr>
2022-04-28 00:06:56 +0200

    Create migrations and add admin rights on comment model

--- topoguide/itineraires/admin.py
+++ topoguide/itineraires/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Itineraire, Sortie
+from .models import Itineraire, Sortie, Comment
 
 # Access to the models for the admin users to modify or create them
 admin.site.register(Itineraire)
 admin.site.register(Sortie)
+admin.site.register(Comment)
